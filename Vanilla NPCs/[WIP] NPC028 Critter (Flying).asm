b79389ae01297d52ff497b62086ac246bc23deba
Thomas Polese <thomas@polese.us>
2026-09-24 19:30:07 -0700

    commenting out the state code

--- Vanilla NPCs/[WIP] NPC028 Critter (Flying).asm
+++ Vanilla NPCs/[WIP] NPC028 Critter (Flying).asm
@@ -1,10 +1,10 @@
 OFFSET NPC028 ;42BAE0
 ; State 0: adjusts positioning of NPC upon initializing, then goes to state 1
-; State 1: NPC is idle. tests how close the player is. if the player is close, NPC will go to state 2
-; State 2: 
-; State 3: 
+; State 1: NPC is idle. tests how close the player is. if the player is close, NPC will begin to jump
+; State 2: jump startup. wait some frames while idle, then sets the initial jump velocity
+; State 3: jump sequence. NPC will jump until its velocity reaches a certain point, and then transition into its flight state
 ; State 4: 
-; State 5: 
+; State 5: landing sequence. the gravity code will keep running until the NPC hits the ground, at which point it will go back to the idle state
 
 #DEFINE
 
@@ -85,6 +85,7 @@ MOV NPC.ScriptTimer, 0
 :CheckScriptTimerForState1
 CMP NPC.ScriptTimer, WAIT_BEFORE_ATTACK ;if the wait period has not yet passed...
 JL :SetGravity ;...then skip this section
+
 ;;; section 2 - checks distance to see if NPC should be put in its attack state ;;;
 MOV EDX, NPC.X
 SUB EDX, HORIZONTAL_DISTANCE_CHECK_ATTACK
@@ -107,21 +108,20 @@ MOV NPC.FrameNum, 0
 MOV NPC.ScriptTimer, 0
 JMP :SetGravity
 
-;start of jump state
 :State2
 INC NPC.ScriptTimer
-CMP NPC.ScriptTimer, 8
+CMP NPC.ScriptTimer, 8 ;wait this many frames before starting the jump
 JLE :SetGravity
 MOV NPC.ScriptState, 3
-MOV NPC.FrameNum, 2
-MOV NPC.MoveY, -4CC
+MOV NPC.FrameNum, 2 ;jumping frame
+MOV NPC.MoveY, -4CC ;jump velocity
 PUSH 1 
 PUSH 1E 
-CALL PlaySound
+CALL PlaySound ;jump sound effect
 ADD ESP, 8
 SETPOINTER
 MOV EDX, NPC.X
-CMP EDX, PlayerXPos
+CMP EDX, PlayerXPos ;set NPC direction left/right to face the player
 JLE :SetDirectionRightForState2
 MOV NPC.Direction, 0
 JMP :CheckDirectionForState2
@@ -130,7 +130,7 @@ JMP :CheckDirectionForState2
 MOV NPC.Direction, 2
 
 :CheckDirectionForState2
-CMP NPC.Direction, 0
+CMP NPC.Direction, 0 ;using the direction that was just set, set the X velocity in that direction
 JNE :SetRightXVelocityForState2
 MOV NPC.MoveX, -100
 JMP :SetGravity
@@ -139,16 +139,12 @@ JMP :SetGravity
 MOV NPC.MoveX, 100
 JMP :SetGravity
 
-;jump state
 :State3
-;check if Y velocity is low
-CMP NPC.MoveY, 100
+CMP NPC.MoveY, 100 ;wait until the gravity starts bringing the NPC down, per the gravity that is being applied every frame
 JLE :SetGravity
-;save Y position in weird NPC variable
 MOV EDX, NPC.Y
-MOV NPC.CurlyMacro2, EDX
-;set scriptstate to 4
-MOV NPC.ScriptState, 4
+MOV NPC.Directive, EDX ;save the initial Y position of the flight state to use as an anchor point later
+MOV NPC.ScriptState, 4 ;setup for the flight state
 MOV NPC.FrameNum, 3
 MOV NPC.ScriptTimer, 0
 JMP :SetGravity
@@ -228,27 +224,25 @@ JMP :SetGravity
 
 ;landing after flying state
 :State5
-;check if the NPC is... NOT colliding with the floor
 MOV EDX, NPC.Collision
-AND EDX, 00000008
-JE :SetGravity
-MOV NPC.Damage, 2
+AND EDX, 00000008 ;if the NPC is not colliding with the floor...
+JE :SetGravity ;...then continue setting the gravity
+MOV NPC.Damage, 2 ;if the NPC has hit the ground, then reset variables and go back to the idle state
 MOV NPC.MoveX, 0
 MOV NPC.ScriptTimer, 0
 MOV NPC.FrameNum, 0
 MOV NPC.ScriptState, 1
-;play hit the ground sfx
 PUSH 1 
 PUSH 17 
-CALL PlaySound 
+CALL PlaySound ;hit the ground sound effect
 ADD ESP, 8
 SETPOINTER
 
 :SetGravity
-CMP NPC.ScriptState, 4
+CMP NPC.ScriptState, 4 
 JE :CheckXVelocity
-ADD NPC.MoveY, 40
-CMP NPC.MoveY, 5FF
+ADD NPC.MoveY, 40 ;add to the Y velocity to simulate gravity pulling down
+CMP NPC.MoveY, 5FF ;cap falling speed
 JLE :AddVelocitiesToPositions
 MOV NPC.MoveY, 5FF
 JMP :AddVelocitiesToPositions
@@ -265,7 +259,7 @@ SUB NPC.MoveX, 20
 
 :CheckYVelocity
 MOV EDX, NPC.Y
-CMP EDX, NPC.CurlyMacro2
+CMP EDX, NPC.Directive
 JLE :IncreaseYVelocity
 SUB NPC.MoveY, 10
 JMP :CapPositiveYVelocity
